e662a1ca65894e7f656a014faeeeec863ef6aebb
AidenLu2110 <aidenlu33@gmail.com>
2026-01-02 13:09:13 -0800

    Initial commit: Added XboxController class with MQTT/UDP logic

---
+++ thruster.py
@@ -0,0 +1,140 @@
+import socket
+import os
+import math
+from inputs import get_gamepad, devices
+import threading
+import time
+
+class XboxController(object):
+    Max_Trig_Val = math.pow(2, 8)
+    Max_Joy_Val = math.pow(2, 15)
+
+    def __init__(self, gamepad):
+        self.gamepad = gamepad
+        self.LeftJoystickY = 0
+        self.LeftJoystickX = 0
+        self.RightJoystickY = 0
+        self.RightJoystickX = 0
+        self.LeftTrigger = 0
+        self.RightTrigger = 0
+        self.LeftBumper = 0
+        self.RightBumper = 0
+        self.A = 0
+        self.X = 0
+        self.Y = 0
+        self.B = 0
+        self.LeftThumb = 0
+        self.RightBumper = 0
+        self.Back = 0
+        self.Start = 0
+        self.Start = 0
+        self.LeftDPad = 0
+        self.RightDPad = 0
+        self.UpDPad = 0
+        self.DownDPad = 0
+
+        self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
+        self._monitor_thread.daemon = True
+        self._monitor_thread.start()
+
+    def read(self):
+        x = self.LeftJoystickX
+        y = self.LeftJoystickY
+        a = self.A
+        b = self.X
+        rb = self.RightBumper
+        return [x, y, a, b, rb]
+
+    def apply_deadzone(self, value, dz = 0.05):
+        return 0 if abs(value) < dz else value #deadzones added no stick drift
+
+    def smooth(self, prev, new, factor=0.2):
+        return prev * (1- factor) + new * factor
+
+    def _monitor_controller(self):
+
+
+SERVER_IP = ''
+SERVER_PORT = 
+
+            
+joyROV = XboxController(devices.gamepads[0])
+joyClaw = XboxController(devices.gamepads[1])
+
+pushed = False
+pitchAngle = 0
+yawAngle = 0
+als = False
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+    sock.settimeout(2.0)
+
+    while True:
+        if als:
+            pitchAngle += pow(joyROV.RightJoystickY, 3) * 0.001
+            yawAngle += pow(joyROV.RightJoystickX, 3) * 0.001
+
+        if joyROV.Y == 1 and not pushed:
+            als = not als
+        pushed = (joyROV.Y == 1)
+
+        if pitchAngle < -180:
+            pitchAngle += 360
+        elif pitchAngle > 180:
+            pitchAngle -= 360
+
+        if yawAngle < -180:
+            yawAngle += 360
+        elif yawAngle > 180:
+            yawAngle -= 360
+
+        scale = 0.5
+        if joyROV.A == 1:
+            scale = 1.0
+        if joyROV.B == 1:
+            scale = 0.25
+        if joyROV.X == 1:
+            scale = 1.5
+
+        out = 1 if als else 0
+
+        MESSAGE = (
+            str(joyROV.LeftJoystickY * scale * 1.5) + " " +
+            str(joyROV.LeftJoystickX * scale * -1) + " " +
+            str((joyROV.RightTrigger - joyROV.LeftTrigger) / -3.0 * scale) + " " +
+            str(joyROV.RightJoystickX * 0.66 * scale * -2) + " " +
+            str(joyROV.RightJoystickY * 0.66 * scale * 2) + " " +
+            str((joyROV.RightBumper - joyROV.LeftBumper) * scale) + " " +
+            str(round(pow(joyClaw.RightJoystickY, 3), 1) * 0.4) + " " +
+            str(int(joyClaw.B) - int(joyClaw.A) * 1.4) + " " +
+            str(round(pow(joyClaw.RightJoystickX, 3), 1) * 0.15) + " " +
+            str(pow(joyClaw.LeftJoystickY, 3) * -0.25) + " " +
+            str(pitchAngle) + " " +
+            str(yawAngle) + " " +
+            str(out)
+        )
+
+        print(
+            f"X: {joyROV.LeftJoystickY:.2f}",
+            f"Y: {joyROV.LeftJoystickX:.2f}",
+            f"Z: {(joyROV.RightTrigger - joyROV.LeftTrigger) / 4.0:.2f}",
+            f"Roll: {joyROV.RightBumper - joyROV.LeftBumper}",
+            f"Pitch: {joyROV.RightJoystickY:.2f}",
+            f"Yaw: {joyROV.RightJoystickX:.2f}",
+            f"Claw Pitch: {pow(joyClaw.RightJoystickY * -1, 3):.1f}",
+            f"Claw Open: {joyClaw.RightTrigger - joyClaw.LeftTrigger:.1f}",
+            f"Claw Yaw: {joyClaw.RightJoystickX:.1f}",
+            f"Claw Rotate: {joyClaw.LeftJoystickX:.1f}",
+            f"PitchAngle: {pitchAngle:.1f}",
+            f"YawAngle: {yawAngle:.1f}",
+            als
+        )
+
+        sock.sendto(MESSAGE.encode(), (SERVER_IP, SERVER_PORT))
+
+        time.sleep(0.01)
+
+
+
+
+
