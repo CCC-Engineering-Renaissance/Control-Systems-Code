98c5edc7d56843f99cc970c5bc156c64b1e5c14a
ZachDrowsy <ztdavidson14@gmail.com>
2026-01-16 18:44:54 -0800

    Added to Thruster.cpp

--- THRUSTER_PCA9685/src/Thruster.cpp
+++ THRUSTER_PCA9685/src/Thruster.cpp
@@ -3,13 +3,24 @@
 #include <algorithm>
 #include <cmath>
 #include <stdexcept>
+using std::clamp;    //Just because I can... (see result on line 22)
 
 namespace {
   constexpr int MIN_PIN = 0;
   constexpr int MAX_PIN = 15;
 
-//  Driver Code is written as if PWM values are in ms so we need to convert the values as we write PWM
-inline double us_to_ms(int us) { 
-  return static_cast<double>(us) / 1000.0; 
-  }
+  //  Driver Code is written as if PWM values are in ms so we need to convert the values as we write PWM
+  inline double us_to_ms(int us) { 
+    return static_cast<double>(us) / 1000.0; 
+    }
 } 
+
+    // Helper checks and clamps
+bool Thruster::isCorrectPin(int p) const{
+  return (p >= 0 && p <= 15);
+}
+double clampPower(double power) const;
+int Thruster::clampPWM(int pwm_us) const{
+  int clamped = clamp()
+}
+
