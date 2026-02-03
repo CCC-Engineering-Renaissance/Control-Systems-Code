f751a9a87aca70973f0f3d1e8f97eb0448557f1c
jalhayan <aleenjalhayan06@gmail.com>
2026-02-02 20:53:55 -0800

    created main for camera, added flags and changed logic for camerSender.py

--- camera/cameraSender.py
+++ camera/cameraSender.py
@@ -2,48 +2,79 @@ import cv2
 import configCamera as config 
 import threading
 
+
+#script requires gstreamer and its plugins to be installed on pi DONT FORGET
+
 class cameraROV:
     def __init__(self, cameraID, port):
+
+        #making flag to make sure code only runs for a respective camera if it detects it exists
+        self.initSuccess = False
         self.cameraID = cameraID #tells you the camera number
         self.port = port # what port the respective camera gets sent to 
         self.cap = cv2.VideoCapture(self.cameraID)
+        
 
-        if not self.cap.isOpened():
-            print("Error")
+        ret, frame = self.cap.read()
+        if self.cap.isOpened() and ret:
+            print(f"Camera {self.cameraID} is open and sending data")
+            for prop, value in config.propertyMap.items():
+                self.cap.set(prop, value)
+           
+            #this is the gstreamer pipeline string that converts the frames from the if statement into a stream in the network
+            #if video is blurry/pixelated increase bitrate (you can do this in configCamera.py)
+            #if video is laggy/delayed lower bitrate
+            #if pi is getting hot ensure ultrafast is on and maybe lower resolution
+            streamConvert = (
+                f"appsrc ! videoconvert ! x264enc tune={config.tunePreset} speed-preset={config.speedPreset} bitrate={config.bitRate} ! "
+                f"rtph264pay config-interval=1 pt=96 ! "
+                f"queue ! "
+                f"udpsink host={config.laptopIPAddress} port={self.port}"
+            )
         
+            #tells the computer how to prepare for the data sent from streamConvert
+            self.out = cv2.VideoWriter(streamConvert, cv2.CAP_GSTREAMER, 0, 30, (config.frameWidth, config.frameHeight))
         
-        #this is the gstreamer pipeline string that converts the frames from the if statement into a stream in the network
-        #if video is blurry/pixelated increase bitrate (you can do this in configCamera.py)
-        #if video is laggy/delayed lower bitrate
-        #if pi is getting hot ensure ultrafast is on and maybe lower resolution
-        streamConvert = (
-            f"appsrc ! videoconvert ! x264enc tune={config.tunePreset} speed-preset={config.speedPreset} bitrate={config.bitRate} ! "
-            f"rtph264pay config-interval=1 pt=96 ! "
-            F"queue ! "
-            f"udpsink host={config.laptopIPAddress} port={self.port}"
-        )
+            #when the code prior to this succeeds it will signal a true flag instead of false
+            #this way the code wont just send data to a camera that isnt even being detected
         
-        #tells the computer how to prepare for the data sent from streamConvert
-        self.out = cv2.VideoWriter(streamConvert, cv2.CAP_GSTREAMER, 0, 30, (config.frameWidth, config.frameHeight))
+            self.initSuccess = True 
+            #should print when completed the function per each camera
+            print(f"Setup complete for {self.cameraID}")
+
+
+        else:
+            print(f"Error: initial setup for camera {self.cameraID} failed")
         
-        #should print when completed the function per each camera
-        print(f"Setup complete for {self.cameraID}")
+
     
         #basically acts like a stop motion film but that is how we are able to send the camera data to the computer 
     def stream(self):
-        while True: 
-            ret, frame = self.cap.read() #takes the photo
+        try: 
+            if not self.initSuccess:
+                raise Exception("Initialization was False")
 
-            if ret:
-                self.out.write(frame) #sends photo to laptop
-            else:
-                print(f"Error: Camera {self.cameraID} disconnected.")
-                break
+            while True: 
+                ret, frame = self.cap.read() #takes the photo
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+                if ret:
+                    self.out.write(frame)
+                else:
+                    print(f"Error: Camera {self.cameraID} disconnected.")
+                    break
+                #press q to quit
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
 
-        #only happens when loop breaks   
+
+        except Exception as e:
+            print(f"Stream blocked for Camera {self.cameraID}: {e}")
+
+
+        #only happens when loop breaks
+
+        #grabs image data
         self.cap.release()
-        self.out.release()
-
+        if self.initSuccess:
+            self.out.release() #converts image data
+            
