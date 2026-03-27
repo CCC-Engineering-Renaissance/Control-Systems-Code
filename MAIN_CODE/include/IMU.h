62baf92b06b5ea24f0450e89c8013f28aff55eda
ZachDrowsy <ztdavidson14@gmail.com>
2026-03-27 13:51:20 -0700

    Started IMU class

---
+++ MAIN_CODE/include/IMU.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "MPU6050.h"
+
+class IMU {
+
+private:
+  
+  MPU6050 sensor;
+
+  float ax, ay, az;
+  float gx, gy, gz;
+  float roll, pitch, yaw 
+
+public:
+
+    IMU();
+    explicit IMU(int8_t address);
+
+    void update();
+
+    float getAccelX() const;
+    float getAccelY() const;
+    float getAccelZ() const;
+
+    float getGyroX() const;
+    float getGyroY() const;
+    float getGyroZ() const;
+
+    float getRoll() const;
+    float getPitch() const;
+    float getYaw() const;
+
+};
